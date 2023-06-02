23b9f08ecb79c429df980a379ac052ea0e0dc6a1
Sergio Ceron <sceronf@gmail.com>
2023-06-02 10:12:22 -0600

    Default export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+import { GasModelSigner } from "./signer";
+import { GasModelProvider } from "./provider";
+
 export { GasModelSigner } from "./signer";
 export { GasModelProvider } from "./provider";
+
+export default { GasModelSigner, GasModelProvider };
--- tests/index.ts
+++ tests/index.ts
@@ -1,24 +1,24 @@
 import 'mocha';
 import {ethers} from "ethers";
 import * as chai from "chai";
-import {GasModelProvider, GasModelSigner} from "../src";
-import {RegisterHashContract} from "./fixtures";
+import lacchain from "../src";
+import {PKD} from "./fixtures";
 
 const expect = chai.expect;
 chai.should();
 
 describe('Tests', () => {
 
-        let contractAddress = "";
-        const provider = new GasModelProvider('http://34.69.22.82')
+        let contractAddress = "0xf6F2B3665E5A596780A4Ee9247A1cA409bd9Edc2";
+        const provider = new lacchain.GasModelProvider('http://34.73.228.200')
         const privateKey = '0E273FFD9CF5214F7D6ADE5D1ABFD6D101B648AF12BC2DE6AC4AFCB4DB805CD3'
-        const nodeAddress = '0xd00e6624a73f88b39f82ab34e8bf2b4d226fd768';
+        const nodeAddress = '0x971bb94d235a4ba42d53ab6fb0a86b12c73ba460';
         const expiration = 1736394529;
-        const signer = new GasModelSigner(privateKey, provider, nodeAddress, expiration);
+        const signer = new lacchain.GasModelSigner(privateKey, provider, nodeAddress, expiration);
 
         it('should deploy contract', async () => {
-            const factory = new ethers.ContractFactory(RegisterHashContract.abi, RegisterHashContract.bytecode, signer)
-            const contract = await factory.deploy({gasLimit: 100000, gasPrice: 0})
+            const factory = new ethers.ContractFactory(PKD.abi, PKD.bytecode, signer)
+            const contract = await factory.deploy({gasLimit: 1588811, gasPrice: 0})
             const receipt = await contract.deployTransaction.wait();
             console.log(`Deployment successful! Contract Address: ${receipt.contractAddress}`);
             expect(receipt.contractAddress).to.not.null;
@@ -27,13 +27,9 @@ describe('Tests', () => {
         });
 
         it('should send a transaction', async () => {
-            const hash = "0x7465737400000000000000000000000000000000000000000000000000000000";
-            const contract = new ethers.Contract(contractAddress, RegisterHashContract.abi, signer);
-            const tx = await contract.store(hash);
-            const receipt = await tx.wait();
-            console.log('Receipt', receipt);
-            const stored = await contract.retreiveHash('0x173CF75f0905338597fcd38F5cE13E6840b230e9');
-            expect(stored).to.be.equals(hash);
+            const contract = new ethers.Contract(contractAddress, PKD.abi, signer);
+            const stored = await contract.publicKeys('0x173CF75f0905338597fcd38F5cE13E6840b230e9');
+            expect(stored).to.be.equals("");
             return false;
         });
     }
